1b1a150c3b6704895286ac5aee0de03bf8625b9e
joostlawerman <joostlawerman@users.noreply.github.com>
2019-05-20 15:31:11 +0200

    [ch92] WIP

---
+++ migrations/2019-05-20 IR20 TRIGGER_PLAYERS_FROM_MATCH_SHOULD_BE_IN_POULE.sql
@@ -0,0 +1,89 @@
+-- +migrate Up 
+CREATE OR ALTER TRIGGER TRIGGER_CHESSMATCH_PLAYERS_FROM_MATCH_SHOULD_BE_IN_POULE
+    ON CHESSMATCH
+    AFTER INSERT, UPDATE
+AS
+    BEGIN
+        IF @@ROWCOUNT = 0
+            RETURN
+        
+        SET NOCOUNT ON
+
+        BEGIN TRY
+            IF NOT EXISTS(
+                SELECT 1
+                FROM TOURNAMENT_PLAYER_OF_POULE tp_of_p
+                WHERE EXISTS (
+                    SELECT 1
+                    FROM Inserted i
+                    WHERE i.chessclubname = tp_of_p.chessclubname
+                        AND i.tournamentname = tp_of_p.tournamentname
+                        AND i.roundnumber = tp_of_p.roundnumber
+                        AND i.pouleno = tp_of_p.pouleno
+                        AND (
+                            i.playeridwhite = tp_of_p.playerid
+                            OR i.playeridblack = tp_of_p.playerid
+                        )
+                )
+                GROUP BY tp_of_p.chessclubname,
+                    tp_of_p.tournamentname,
+                    tp_of_p.roundnumber,
+                    tp_of_p.pouleno
+                HAVING COUNT(*) = 2
+            )
+                BEGIN
+                    THROW 50005, 'Player should be in the poule of the chess match.', 1
+                END
+        END TRY
+        BEGIN CATCH
+            THROW
+        END CATCH
+    END
+;
+
+-- +migrate Down
+DROP TRIGGER TRIGGER_CHESSMATCH_PLAYERS_FROM_MATCH_SHOULD_BE_IN_POULE
+;
+
+-- +migrate Up 
+CREATE OR ALTER TRIGGER TRIGGER_PLAYER_TOURNAMENT_OF_POULE_PLAYERS_FROM_MATCH_SHOULD_BE_IN_POULE
+    ON TOURNAMENT_PLAYER_OF_POULE
+    AFTER INSERT, UPDATE
+AS
+    BEGIN
+        IF @@ROWCOUNT = 0
+            RETURN
+        
+        SET NOCOUNT ON
+
+        BEGIN TRY
+            IF EXISTS(
+                SELECT 1
+                FROM CHESSMATCH cm
+                WHERE EXISTS (
+                    SELECT 1
+                    FROM Inserted i
+                    WHERE cm.chessclubname = i.chessclubname
+                        AND cm.tournamentname = i.tournamentname
+                        AND cm.roundnumber = i.roundnumber
+                        AND cm.pouleno = i.pouleno
+                        AND (
+                            cm.playeridwhite = i.playerid
+                            OR cm.playeridblack = i.playerid
+                        )
+                )
+            )
+                BEGIN
+                    THROW 50005, 'Player is already in a match within its current poule.', 1
+                END
+        END TRY
+        BEGIN CATCH
+            THROW
+        END CATCH
+    END
+;
+
+-- +migrate Down
+DROP TRIGGER TRIGGER_PLAYER_TOURNAMENT_OF_POULE_PLAYERS_FROM_MATCH_SHOULD_BE_IN_POULE
+;
+
---
+++ migrations/2019-05-20 IR20 TRIGGER_PLAYERS_FROM_MATCH_SHOULD_BE_IN_POULE.test.sql
@@ -0,0 +1,191 @@
+-- +migrate Up
+EXEC tSQLt.NewTestClass 'ir20';
+
+-- +migrate Down
+EXEC tSQLt.DropClass 'ir20';
+
+-- +migrate Up
+CREATE OR ALTER PROCEDURE [ir20].[test_player_of_chess_match_is_in_poule]
+AS
+BEGIN
+	--Assemble
+	EXEC tSQLt.FakeTable 'dbo', 'CHESSMATCH'
+	EXEC tSQLt.FakeTable 'dbo', 'TOURNAMENT_PLAYER_OF_POULE'
+
+	EXEC [tSQLt].[ApplyTrigger] @tablename = 'dbo.CHESSMATCH', @triggername = 'TRIGGER_CHESSMATCH_PLAYERS_FROM_MATCH_SHOULD_BE_IN_POULE'
+
+    INSERT INTO TOURNAMENT_PLAYER_OF_POULE (
+        chessclubname, 
+        tournamentname,
+        roundnumber,
+        pouleno,
+        playerid
+    ) VALUES (
+        'Arnhemse Schaak Club', 'In het Gemaal', 1, 10, 300
+    )
+
+    INSERT INTO TOURNAMENT_PLAYER_OF_POULE (
+        chessclubname, 
+        tournamentname,
+        roundnumber,
+        pouleno,
+        playerid
+    ) VALUES (
+        'Arnhemse Schaak Club', 'In het Gemaal', 1, 10, 200
+    )
+
+    EXEC tSQLt.ExpectNoException
+    
+    INSERT INTO CHESSMATCH (
+        chessclubname, 
+        tournamentname,
+        roundnumber,
+        pouleno,
+        playeridwhite,
+        playeridblack
+    ) VALUES (
+        'Arnhemse Schaak Club', 'In het Gemaal', 1, 10, 300, 200
+    )
+END
+;
+
+-- +migrate Up
+CREATE OR ALTER PROCEDURE [ir20].[test_player_of_chess_match_is_not_in_poule]
+AS
+BEGIN
+	--Assemble
+	EXEC tSQLt.FakeTable 'dbo', 'CHESSMATCH'
+	EXEC tSQLt.FakeTable 'dbo', 'TOURNAMENT_PLAYER_OF_POULE'
+
+	EXEC [tSQLt].[ApplyTrigger] @tablename = 'dbo.CHESSMATCH', @triggername = 'TRIGGER_CHESSMATCH_PLAYERS_FROM_MATCH_SHOULD_BE_IN_POULE'
+
+    INSERT INTO TOURNAMENT_PLAYER_OF_POULE (
+        chessclubname, 
+        tournamentname,
+        roundnumber,
+        pouleno,
+        playerid
+    ) VALUES (
+        'Arnhemse Schaak Club', 'In het Gemaal', 1, 10, 300
+    )
+
+    INSERT INTO TOURNAMENT_PLAYER_OF_POULE (
+        chessclubname, 
+        tournamentname,
+        roundnumber,
+        pouleno,
+        playerid
+    ) VALUES (
+        'Arnhemse Schaak Club', 'In het Gemaal', 1, 8, 200
+    )
+
+    EXEC tSQLt.ExpectException @ExpectedMessage = 'Player should be in the poule of the chess match.', @ExpectedSeverity = 16, @ExpectedErrorNumber = 50005
+    
+    INSERT INTO CHESSMATCH (
+        chessclubname, 
+        tournamentname,
+        roundnumber,
+        pouleno,
+        playeridwhite,
+        playeridblack
+    ) VALUES (
+        'Arnhemse Schaak Club', 'In het Gemaal', 1, 10, 300, 200
+    )
+END
+;
+
+-- +migrate Up
+CREATE OR ALTER PROCEDURE [ir20].[test_player_is_within_chessmatch]
+AS
+BEGIN
+	--Assemble
+	EXEC tSQLt.FakeTable 'dbo', 'CHESSMATCH'
+	EXEC tSQLt.FakeTable 'dbo', 'TOURNAMENT_PLAYER_OF_POULE'
+
+	EXEC [tSQLt].[ApplyTrigger] @tablename = 'dbo.TOURNAMENT_PLAYER_OF_POULE', @triggername = 'TRIGGER_PLAYER_TOURNAMENT_OF_POULE_PLAYERS_FROM_MATCH_SHOULD_BE_IN_POULE'
+
+    INSERT INTO CHESSMATCH (
+        chessclubname, 
+        tournamentname,
+        roundnumber,
+        pouleno,
+        playeridwhite,
+        playeridblack
+    ) VALUES (
+        'Arnhemse Schaak Club', 'In het Gemaal', 1, 10, 300, 200
+    )
+
+    INSERT INTO TOURNAMENT_PLAYER_OF_POULE (
+        chessclubname, 
+        tournamentname,
+        roundnumber,
+        pouleno,
+        playerid
+    ) VALUES (
+        'Arnhemse Schaak Club', 'In het Gemaal', 1, 10, 300
+    )
+
+    INSERT INTO TOURNAMENT_PLAYER_OF_POULE (
+        chessclubname, 
+        tournamentname,
+        roundnumber,
+        pouleno,
+        playerid
+    ) VALUES (
+        'Arnhemse Schaak Club', 'In het Gemaal', 1, 10, 200
+    )
+
+    EXEC tSQLt.ExpectException @ExpectedMessage = 'Player is already in a match within its current poule.', @ExpectedSeverity = 16, @ExpectedErrorNumber = 50005
+
+    UPDATE TOURNAMENT_PLAYER_OF_POULE
+        SET pouleno = 8
+    WHERE chessclubname = 'Arnhemse Schaak Club'
+        AND tournamentname = 'In het Gemaal'
+        AND roundnumber =  1
+        AND playerid = 200
+END
+;
+
+
+
+-- +migrate Up
+CREATE OR ALTER PROCEDURE [ir20].[test_player_has_no_chessmatch]
+AS
+BEGIN
+	--Assemble
+	EXEC tSQLt.FakeTable 'dbo', 'CHESSMATCH'
+	EXEC tSQLt.FakeTable 'dbo', 'TOURNAMENT_PLAYER_OF_POULE'
+
+	EXEC [tSQLt].[ApplyTrigger] @tablename = 'dbo.TOURNAMENT_PLAYER_OF_POULE', @triggername = 'TRIGGER_PLAYER_TOURNAMENT_OF_POULE_PLAYERS_FROM_MATCH_SHOULD_BE_IN_POULE'
+
+    INSERT INTO TOURNAMENT_PLAYER_OF_POULE (
+        chessclubname, 
+        tournamentname,
+        roundnumber,
+        pouleno,
+        playerid
+    ) VALUES (
+        'Arnhemse Schaak Club', 'In het Gemaal', 1, 10, 300
+    )
+
+    INSERT INTO TOURNAMENT_PLAYER_OF_POULE (
+        chessclubname, 
+        tournamentname,
+        roundnumber,
+        pouleno,
+        playerid
+    ) VALUES (
+        'Arnhemse Schaak Club', 'In het Gemaal', 1, 10, 200
+    )
+
+    EXEC tSQLt.ExpectNoException
+
+    UPDATE TOURNAMENT_PLAYER_OF_POULE
+        SET pouleno = 8
+    WHERE chessclubname = 'Arnhemse Schaak Club'
+        AND tournamentname = 'In het Gemaal'
+        AND roundnumber =  1
+        AND playerid = 200
+END
+;
+
