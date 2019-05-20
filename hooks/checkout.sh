621cd9f92c14c8509cb1a9a7e6a8342479d02271
Jelmergu <github@jelmerwijnja.nl>
2019-05-20 16:14:32 +0200

    fix migrate down again

--- hooks/checkout.sh
+++ hooks/checkout.sh
@@ -3,4 +3,4 @@
 rootDir=$(git rev-parse --show-toplevel)
 pwd="${PWD}"
 
-(cd "$(git rev-parse --show-toplevel)" && exec sql-migrate down) && git checkout ${@} && (cd "$(git rev-parse --show-toplevel)" && exec sql-migrate up)
+(cd "$(git rev-parse --show-toplevel)" && exec sql-migrate down -limit=0) && git checkout ${@} && (cd "$(git rev-parse --show-toplevel)" && exec sql-migrate up)
