92fa7b3b164a6564d5b593e048b10eb1971268e8
R0vi <ivorstaatsl0l@gmail.com>
2019-05-20 15:28:22 +0200

    Updated migration things

--- migrations/2019-05-20 CK_ROUND_STARTS_BEFORE_ENDS.test.sql
+++ migrations/2019-05-20 CK_ROUND_STARTS_BEFORE_ENDS.test.sql
@@ -1,57 +1,45 @@
--- +migrate up
+-- +migrate Up
 EXEC tSQLt.NewTestClass 'IR13';
 
--- +migrate down
+-- +migrate Down
 EXEC tSQLt.DropClass 'IR13';
 
---
---test 1
---
-
--- +migrate up
+-- +migrate Up
 CREATE OR ALTER PROCEDURE [IR13].[test start is before end]
 AS
 BEGIN
 	--Assemble
 	EXEC tSQLt.FakeTable 'dbo', 'TOURNAMENT_ROUND'
 
-	EXEC tSQLt.ApplyConstraint @SchemaName= 'dbo', @Tablename = 'TOURNAMENT_ROUND', @ConstraintName = 'CK_ROUND_STARTS_BEFORE_ENDS';
+	EXEC tSQLt.ApplyConstraint @SchemaName= 'dbo', @Tablename = 'TOURNAMENT_ROUND', @ConstraintName = 'CK_ROUND_STARTS_BEFORE_ENDS'
 
 	EXEC tSQLt.ExpectException
 	--Act
 	INSERT INTO TOURNAMENT_ROUND (starts, ends) VALUES ('2019-05-20', '2019-05-19')
 END;
 
---
---test 2
---
-
--- +migrate up
+-- +migrate Up
 CREATE OR ALTER PROCEDURE [IR13].[test start is after end]
 AS
 BEGIN
 	--Assemble
 	EXEC tSQLt.FakeTable 'dbo', 'TOURNAMENT_ROUND'
 
-	EXEC tSQLt.ApplyConstraint @SchemaName= 'dbo', @Tablename = 'TOURNAMENT_ROUND', @ConstraintName = 'CK_ROUND_STARTS_BEFORE_ENDS';
+	EXEC tSQLt.ApplyConstraint @SchemaName= 'dbo', @Tablename = 'TOURNAMENT_ROUND', @ConstraintName = 'CK_ROUND_STARTS_BEFORE_ENDS'
 
 	EXEC tSQLt.ExpectNoException
 	--Act
 	INSERT INTO TOURNAMENT_ROUND (starts, ends) VALUES ('2019-05-20', '2019-05-25')
 END;
 
---
---test 3
---
-
--- +migrate up
+-- +migrate Up
 CREATE OR ALTER PROCEDURE [IR13].[test start is same time end]
 AS
 BEGIN
 	--Assemble
 	EXEC tSQLt.FakeTable 'dbo', 'TOURNAMENT_ROUND'
 
-	EXEC tSQLt.ApplyConstraint @SchemaName= 'dbo', @Tablename = 'TOURNAMENT_ROUND', @ConstraintName = 'CK_ROUND_STARTS_BEFORE_ENDS';
+	EXEC tSQLt.ApplyConstraint @SchemaName= 'dbo', @Tablename = 'TOURNAMENT_ROUND', @ConstraintName = 'CK_ROUND_STARTS_BEFORE_ENDS'
 
 	EXEC tSQLt.ExpectException
 	--Act
