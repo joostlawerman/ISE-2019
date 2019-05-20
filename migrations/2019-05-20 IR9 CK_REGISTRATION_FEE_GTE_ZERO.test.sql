bfd374ad1bd9e87f00d478c32267cda186d1947e
LucaHogeweide <lucahogeweide@gmail.com>
2019-05-20 14:15:12 +0200

    Changes and an extra text.

--- migrations/2019-05-20 IR9 CK_REGISTRATION_FEE_GTE_ZERO.test.sql
+++ migrations/2019-05-20 IR9 CK_REGISTRATION_FEE_GTE_ZERO.test.sql
@@ -22,6 +22,25 @@ END
 GO
 
 --+migrate Up
+CREATE or ALTER PROCEDURE [testIR9].[test_registrationfee_equal_to_zero]
+AS
+BEGIN
+	--Assemble
+	EXEC tSQLt.FakeTable 'dbo', 'TOURNAMENT';
+
+	EXEC tSQLt.ApplyConstraint @SchemaName= 'dbo', @Tablename = 'TOURNAMENT', @ConstraintName = 'CHK_REGISTRATION_FEE_GTE_ZERO';
+
+	--Assert
+	EXEC tSQLt.ExpectNoException 
+
+	--Act
+	insert into 
+	TOURNAMENT(chessclubname,tournamentname,winner,contactname,starts,ends,registrationfee,addressline1,postalcode,city,registrationdeadline)
+	values(null,null,null,null,null,null,0.00,null,null,null,null);
+END
+GO
+
+--+migrate Up
 CREATE or ALTER PROCEDURE [testIR9].[test_registrationfee_lower_than_zero]
 AS
 BEGIN
