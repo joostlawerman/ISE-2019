7da975725a50812b93b9e4cbb7aa4b5629c15c9b
LucaHogeweide <lucahogeweide@gmail.com>
2019-05-20 13:54:55 +0200

    Check constraint for IR9 written and tested with TSQLT tests for 2 scenario's

---
+++ migrations/2019-05-20 IR9 CK_REGISTRATION_FEE_GTE_ZERO.test.sql
@@ -0,0 +1,43 @@
+-- +migrate Up
+EXEC tSQLt.NewTestClass 'testIR9';
+GO
+
+--+migrate Up
+CREATE or ALTER PROCEDURE [testIR9].[test_registrationfee_greater_than_zero]
+AS
+BEGIN
+	--Assemble
+	EXEC tSQLt.FakeTable 'dbo', 'TOURNAMENT';
+
+	EXEC tSQLt.ApplyConstraint @SchemaName= 'dbo', @Tablename = 'TOURNAMENT', @ConstraintName = 'CHK_REGISTRATION_FEE_GTE_ZERO';
+
+	--Assert
+	EXEC tSQLt.ExpectNoException 
+
+	--Act
+	insert into 
+	TOURNAMENT(chessclubname,tournamentname,winner,contactname,starts,ends,registrationfee,addressline1,postalcode,city,registrationdeadline)
+	values(null,null,null,null,null,null,1.00,null,null,null,null);
+END
+GO
+
+--+migrate Up
+CREATE or ALTER PROCEDURE [testIR9].[test_registrationfee_lower_than_zero]
+AS
+BEGIN
+	--Assemble
+	EXEC tSQLt.FakeTable 'dbo', 'TOURNAMENT';
+
+	EXEC tSQLt.ApplyConstraint @SchemaName= 'dbo', @Tablename = 'TOURNAMENT', @ConstraintName = 'CHK_REGISTRATION_FEE_GTE_ZERO';
+
+	--Assert
+	EXEC tSQLt.ExpectException 
+
+	--Act
+	insert into 
+	TOURNAMENT(chessclubname,tournamentname,winner,contactname,starts,ends,registrationfee,addressline1,postalcode,city,registrationdeadline)
+	values(null,null,null,null,null,null,-1.00,null,null,null,null);
+END
+GO
+
+EXEC [tSQLt].[RunAll]
