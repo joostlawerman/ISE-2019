7bef1b0d6a85e2ad7f1196999936db6d3b1abd0b
Jasmijn Bartelds <jasmijn.bartelds@hotmail.nl>
2019-05-21 11:11:06 +0200

    ch91 can not change poule when the match has started

---
+++ migrations/2019-05-21 IR15 TRIGGER_PREVENT_CHANGE_OF_POULES_AFTER_MATCH_HAS_BEEN_CREATED.sql
@@ -0,0 +1,27 @@
+-- +migrate Up
+CREATE TRIGGER TRIGGER_PREVENT_CHANGE_OF_POULES_AFTER_MATCH_HAS_BEEN_CREATED
+ON TOURNAMENT_PLAYER_OF_POULE
+AFTER INSERT, UPDATE
+AS
+IF @@ROWCOUNT = 0
+    RETURN
+        
+SET NOCOUNT ON
+BEGIN TRY
+			IF EXISTS (	SELECT 1
+						FROM Inserted i INNER JOIN TOURNAMENT_PLAYER_OF_POULE t ON i.pouleno = t.pouleno
+						WHERE EXISTS(
+									SELECT 1
+									FROM POULE p INNER JOIN CHESSMATCH c ON p.pouleno = c.pouleno
+									WHERE p.pouleno = t.pouleno OR c.result IS NULL))
+
+      		BEGIN
+              	   THROW 50000, 'A poule can not be changed when a match in that poule has started', 1
+      		END
+END TRY
+BEGIN CATCH
+   	THROW
+END CATCH;
+
+-- +migrate Down
+DROP TRIGGER TRIGGER_PREVENT_CHANGE_OF_POULES_AFTER_MATCH_HAS_BEEN_CREATED;
