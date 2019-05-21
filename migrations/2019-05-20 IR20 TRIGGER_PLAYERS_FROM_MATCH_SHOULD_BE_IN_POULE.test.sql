8a37cf29db1171e3fb7966fba41e07f3eb0e88b5
joostlawerman <joostlawerman@users.noreply.github.com>
2019-05-21 13:49:17 +0200

    Fixes

--- migrations/2019-05-20 IR20 TRIGGER_PLAYERS_FROM_MATCH_SHOULD_BE_IN_POULE.test.sql
+++ migrations/2019-05-20 IR20 TRIGGER_PLAYERS_FROM_MATCH_SHOULD_BE_IN_POULE.test.sql
@@ -22,15 +22,7 @@ BEGIN
         playerid
     ) VALUES (
         'Arnhemse Schaak Club', 'In het Gemaal', 1, 10, 300
-    )
-
-    INSERT INTO TOURNAMENT_PLAYER_OF_POULE (
-        chessclubname, 
-        tournamentname,
-        roundnumber,
-        pouleno,
-        playerid
-    ) VALUES (
+    ), (
         'Arnhemse Schaak Club', 'In het Gemaal', 1, 10, 200
     )
 
@@ -67,15 +59,7 @@ BEGIN
         playerid
     ) VALUES (
         'Arnhemse Schaak Club', 'In het Gemaal', 1, 10, 300
-    )
-
-    INSERT INTO TOURNAMENT_PLAYER_OF_POULE (
-        chessclubname, 
-        tournamentname,
-        roundnumber,
-        pouleno,
-        playerid
-    ) VALUES (
+    ), (
         'Arnhemse Schaak Club', 'In het Gemaal', 1, 8, 200
     )
 
@@ -93,101 +77,3 @@ BEGIN
     )
 END
 ;
-
--- +migrate Up
-CREATE PROCEDURE [ir20].[test_player_is_within_chessmatch]
-AS
-BEGIN
-	--Assemble
-	EXEC tSQLt.FakeTable 'dbo', 'CHESSMATCH'
-	EXEC tSQLt.FakeTable 'dbo', 'TOURNAMENT_PLAYER_OF_POULE'
-
-	EXEC [tSQLt].[ApplyTrigger] @tablename = 'dbo.TOURNAMENT_PLAYER_OF_POULE', @triggername = 'TRIGGER_PLAYER_TOURNAMENT_OF_POULE_PLAYERS_FROM_MATCH_SHOULD_BE_IN_POULE'
-
-    INSERT INTO CHESSMATCH (
-        chessclubname, 
-        tournamentname,
-        roundnumber,
-        pouleno,
-        playeridwhite,
-        playeridblack
-    ) VALUES (
-        'Arnhemse Schaak Club', 'In het Gemaal', 1, 10, 300, 200
-    )
-
-    INSERT INTO TOURNAMENT_PLAYER_OF_POULE (
-        chessclubname, 
-        tournamentname,
-        roundnumber,
-        pouleno,
-        playerid
-    ) VALUES (
-        'Arnhemse Schaak Club', 'In het Gemaal', 1, 10, 300
-    )
-
-    INSERT INTO TOURNAMENT_PLAYER_OF_POULE (
-        chessclubname, 
-        tournamentname,
-        roundnumber,
-        pouleno,
-        playerid
-    ) VALUES (
-        'Arnhemse Schaak Club', 'In het Gemaal', 1, 10, 200
-    )
-
-
-    
-    EXEC tSQLt.ExpectException @ExpectedMessage = 'You cannnot change the poule of a player when the player is already in a match within it''s current poule.', @ExpectedSeverity = 16, @ExpectedErrorNumber = 50005
-
-    UPDATE TOURNAMENT_PLAYER_OF_POULE
-        SET pouleno = 8
-    WHERE chessclubname = 'Arnhemse Schaak Club'
-        AND tournamentname = 'In het Gemaal'
-        AND roundnumber =  1
-        AND playerid = 200
-END
-;
-
-
-
--- +migrate Up
-CREATE PROCEDURE [ir20].[test_player_has_no_chessmatch]
-AS
-BEGIN
-	--Assemble
-	EXEC tSQLt.FakeTable 'dbo', 'CHESSMATCH'
-	EXEC tSQLt.FakeTable 'dbo', 'TOURNAMENT_PLAYER_OF_POULE'
-
-	EXEC [tSQLt].[ApplyTrigger] @tablename = 'dbo.TOURNAMENT_PLAYER_OF_POULE', @triggername = 'TRIGGER_PLAYER_TOURNAMENT_OF_POULE_PLAYERS_FROM_MATCH_SHOULD_BE_IN_POULE'
-
-    INSERT INTO TOURNAMENT_PLAYER_OF_POULE (
-        chessclubname, 
-        tournamentname,
-        roundnumber,
-        pouleno,
-        playerid
-    ) VALUES (
-        'Arnhemse Schaak Club', 'In het Gemaal', 1, 10, 300
-    )
-
-    INSERT INTO TOURNAMENT_PLAYER_OF_POULE (
-        chessclubname, 
-        tournamentname,
-        roundnumber,
-        pouleno,
-        playerid
-    ) VALUES (
-        'Arnhemse Schaak Club', 'In het Gemaal', 1, 10, 200
-    )
-
-    EXEC tSQLt.ExpectNoException
-
-    UPDATE TOURNAMENT_PLAYER_OF_POULE
-        SET pouleno = 8
-    WHERE chessclubname = 'Arnhemse Schaak Club'
-        AND tournamentname = 'In het Gemaal'
-        AND roundnumber =  1
-        AND playerid = 200
-END
-;
-
