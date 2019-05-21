6b3e1f5fb25d15decde796195d7048f685721cc1
LucaHogeweide <lucahogeweide@gmail.com>
2019-05-21 09:49:55 +0200

    Removed comments and the 'test' in the testclass.

--- migrations/2019-05-20 IR9 CK_REGISTRATION_FEE_GTE_ZERO.test.sql
+++ migrations/2019-05-20 IR9 CK_REGISTRATION_FEE_GTE_ZERO.test.sql
@@ -1,24 +1,21 @@
 -- +migrate Up
-EXEC tSQLt.NewTestClass 'testIR9'
+EXEC tSQLt.NewTestClass 'IR9'
 ;
 
 -- +migrate Down
-EXEC tSQLt.DropClass 'testIR9'
+EXEC tSQLt.DropClass 'IR9'
 ;
 
 --+migrate Up
-CREATE or ALTER PROCEDURE [testIR9].[test_registrationfee_greater_than_zero]
+CREATE PROCEDURE [IR9].[test_registrationfee_greater_than_zero]
 AS
 BEGIN
-	--Assemble
 	EXEC tSQLt.FakeTable 'dbo', 'TOURNAMENT'
 
 	EXEC tSQLt.ApplyConstraint @SchemaName= 'dbo', @Tablename = 'TOURNAMENT', @ConstraintName = 'CHK_REGISTRATION_FEE_GTE_ZERO'
 
-	--Assert
 	EXEC tSQLt.ExpectNoException 
 
-	--Act
 	insert into 
 	TOURNAMENT(chessclubname,tournamentname,winner,contactname,starts,ends,registrationfee,addressline1,postalcode,city,registrationdeadline)
 	values(null,null,null,null,null,null,1.00,null,null,null,null)
@@ -26,18 +23,16 @@ END
 ;
 
 --+migrate Up
-CREATE or ALTER PROCEDURE [testIR9].[test_registrationfee_equal_to_zero]
+CREATE PROCEDURE [IR9].[test_registrationfee_equal_to_zero]
 AS
 BEGIN
-	--Assemble
+
 	EXEC tSQLt.FakeTable 'dbo', 'TOURNAMENT'
 
 	EXEC tSQLt.ApplyConstraint @SchemaName= 'dbo', @Tablename = 'TOURNAMENT', @ConstraintName = 'CHK_REGISTRATION_FEE_GTE_ZERO'
 
-	--Assert
 	EXEC tSQLt.ExpectNoException 
 
-	--Act
 	insert into 
 	TOURNAMENT(chessclubname,tournamentname,winner,contactname,starts,ends,registrationfee,addressline1,postalcode,city,registrationdeadline)
 	values(null,null,null,null,null,null,0.00,null,null,null,null)
@@ -45,18 +40,16 @@ END
 ;
 
 --+migrate Up
-CREATE or ALTER PROCEDURE [testIR9].[test_registrationfee_lower_than_zero]
+CREATE PROCEDURE [IR9].[test_registrationfee_lower_than_zero]
 AS
 BEGIN
-	--Assemble
+
 	EXEC tSQLt.FakeTable 'dbo', 'TOURNAMENT'
 
 	EXEC tSQLt.ApplyConstraint @SchemaName= 'dbo', @Tablename = 'TOURNAMENT', @ConstraintName = 'CHK_REGISTRATION_FEE_GTE_ZERO'
 
-	--Assert
 	EXEC tSQLt.ExpectException 
 
-	--Act
 	insert into 
 	TOURNAMENT(chessclubname,tournamentname,winner,contactname,starts,ends,registrationfee,addressline1,postalcode,city,registrationdeadline)
 	values(null,null,null,null,null,null,-1.00,null,null,null,null)
