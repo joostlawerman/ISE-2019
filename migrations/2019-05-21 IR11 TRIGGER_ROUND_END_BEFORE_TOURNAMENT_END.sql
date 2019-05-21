87cc7a479f81573693a9de9d3069add0ac400d48
LucaHogeweide <lucahogeweide@gmail.com>
2019-05-21 13:35:02 +0200

    Fixed merging issue.

--- migrations/2019-05-21 IR11 TRIGGER_ROUND_END_BEFORE_TOURNAMENT_END.sql
+++ migrations/2019-05-21 IR11 TRIGGER_ROUND_END_BEFORE_TOURNAMENT_END.sql
@@ -8,14 +8,23 @@ CREATE TRIGGER TRIGGER_ROUND_END_BEFORE_TOURNAMENT_END
           IF @@ROWCOUNT = 0
         RETURN
             BEGIN TRY
-				IF UPDATE (ends)
+				IF ((SELECT ends from inserted) is not null)
 				BEGIN
+
+
+					DECLARE @tournamentEnd datetime = (select t.ends
+													   from TOURNAMENT t inner join inserted i 
+														    on t.tournamentname = i.tournamentname 
+														    and t.chessclubname = i.chessclubname)
+
+					IF(@tournamentEnd is null)
+					THROW
 					
-					IF((select ends from inserted) > (select t.ends
-													  from TOURNAMENT t inner join inserted i 
-														   on t.tournamentname = i.tournamentname 
-														   and t.chessclubname = i.chessclubname))
+					IF(@tournamentEnd is null or (select ends from inserted) is null)
+
+					IF((select ends from inserted) > (@tournamentEnd))
 						THROW 50001, 'A tournament round must end before the end of the tournament.',1
+
 				END			
 			END TRY
             BEGIN CATCH
