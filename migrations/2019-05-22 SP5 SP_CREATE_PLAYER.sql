f5ab5db6ad730162b591bd140f5ca01df18431cd
R0vi <ivorstaatsl0l@gmail.com>
2019-05-22 14:18:07 +0200

    delete playerid

--- migrations/2019-05-22 SP5 SP_CREATE_PLAYER.sql
+++ migrations/2019-05-22 SP5 SP_CREATE_PLAYER.sql
@@ -1,6 +1,5 @@
 -- +migrate Up
 CREATE PROCEDURE SP_CREATE_PLAYER
-@playerid int,
 @chessclubname varchar(100),
 @firstname varchar(50),
 @lastname varchar(50),
@@ -19,7 +18,9 @@ BEGIN
     ELSE  
         BEGIN TRANSACTION
 	BEGIN TRY		
-		INSERT INTO PLAYER VALUES (@playerid, @chessclubname, @firstname, @lastname, @addressline1, @postalcode, @city, @birthdate, @email, @gender)
+		--todo genererate playerid :)
+
+		INSERT INTO PLAYER VALUES (@chessclubname, @firstname, @lastname, @addressline1, @postalcode, @city, @birthdate, @email, @gender)
 	END TRY
 	BEGIN CATCH
 		IF @orginTranCount = 0  
