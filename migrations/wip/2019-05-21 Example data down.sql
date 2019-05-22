1b49ac403123ff27d5538431e595930556d2dbf6
Jelmergu <github@jelmerwijnja.nl>
2019-05-22 14:26:22 +0200

    Commit in preparation of a merge
    Signed-off-by: Jelmergu <github@jelmerwijnja.nl>

--- migrations/wip/2019-05-21 Example data down.sql
+++ migrations/wip/2019-05-21 Example data down.sql
@@ -1,4 +1,28 @@
 -- +migrate Down
+DELETE FROM CHESSMATCHMOVE;
+
+-- +migrate Down
+DELETE FROM ROUND_ROBIN_POULE;
+
+-- +migrate Down
+DELETE FROM SPONSOR;
+
+-- +migrate Down
+DELETE FROM TOURNAMENT_PLAYER_OF_POULE;
+
+-- +migrate Down
+DELETE FROM ROLE;
+
+-- +migrate Down
+DELETE FROM CHESSMATCH_OF_POULE;
+
+-- +migrate Down
+DELETE FROM POULE;
+
+-- +migrate Down
+DELETE FROM TOURNAMENT_ROUND;
+
+-- +migrate Down
 DELETE FROM TOURNAMENT_PLAYER;
 
 -- +migrate Down
@@ -6,9 +30,6 @@ DELETE FROM TOURNAMENT;
 
 -- +migrate Down
 DELETE FROM CONTACTPERSON;
-
--- +migrate Down
-DELETE FROM ROLE;
 
 -- +migrate Down
 DELETE FROM PLAYER;
