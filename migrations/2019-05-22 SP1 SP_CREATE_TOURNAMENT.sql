ef8bac67cf3065834b3b231567fc3e108dbed625
R0vi <ivorstaatsl0l@gmail.com>
2019-05-22 11:11:56 +0200

    made procedure and test

---
+++ migrations/2019-05-22 SP1 SP_CREATE_TOURNAMENT.sql
@@ -0,0 +1,39 @@
+-- +migrate Up
+CREATE PROCEDURE SP_CREATE_TOURNAMENT
+@chessclubname varchar(100),
+@tournamentname varchar(100),
+@winner int,
+@contactname varchar(101),
+@starts datetime,
+@ends datetime,
+@registrationfee money,
+@addressline1 varchar(100),
+@postalcode varchar(6),
+@city varchar(100),
+@registrationdeadline datetime
+AS
+BEGIN
+	DECLARE @orginTranCount INT
+	SET  @orginTranCount = @@TRANCOUNT
+	IF @orginTranCount > 0    
+        SAVE TRANSACTION ProcedureSave  
+    ELSE  
+        BEGIN TRANSACTION
+	BEGIN TRY		
+		INSERT INTO TOURNAMENT VALUES (@chessclubname, @tournamentname, @winner, @contactname, @starts, @ends, @registrationfee, @addressline1, @postalcode, @city, @registrationdeadline)
+	END TRY
+	BEGIN CATCH
+		IF @orginTranCount = 0  
+            ROLLBACK TRANSACTION  
+        ELSE  
+			IF XACT_STATE() <> -1  
+            ROLLBACK TRANSACTION ProcedureSave
+		DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE()
+		DECLARE @ErrorSeverity INT = ERROR_SEVERITY()
+        DECLARE @ErrorState INT = ERROR_STATE()
+		RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState)
+	END CATCH
+END;
+
+-- +migrate Down
+DROP PROCEDURE SP_CREATE_TOURNAMENT;
---
+++ migrations/2019-05-22 SP1 SP_CREATE_TOURNAMENT.test.sql
@@ -0,0 +1,35 @@
+-- +migrate Up
+EXEC tSQLt.NewTestClass 'SP1';
+
+-- +migrate Down
+EXEC tSQLt.DropClass 'SP1';
+
+-- +migrate Up
+CREATE PROCEDURE [SP1].[test insert iets]
+AS
+BEGIN
+	--Assemble
+	EXEC tSQLt.FakeTable 'dbo', 'TOURNAMENT';
+
+	--Act
+	EXEC SP_CREATE_TOURNAMENT 'CC1', 'tourny1', 1, 'gekkenaam', '2019-05-22', '2019-05-23', '10', 'blastraat', '1234AB', 'Apeldoorn', '2019-05-22'
+
+	--Assert
+	CREATE TABLE expected (
+    chessclubname        VARCHAR(100) NOT NULL,
+    tournamentname       VARCHAR(100) NOT NULL,
+    winner               INT          NULL,
+    contactname          VARCHAR(101) NOT NULL,
+    starts               DATETIME     NOT NULL,
+    ends                 DATETIME     NULL,
+    registrationfee      MONEY        NOT NULL,
+    addressline1         VARCHAR(100) NOT NULL,
+    postalcode           VARCHAR(6)   NOT NULL,
+    city                 CHAR(100)    NOT NULL,
+    registrationdeadline DATETIME     NOT NULL,
+	)
+
+	INSERT INTO expected VALUES ('CC1', 'tourny1', 1, 'gekkenaam', '2019-05-22', '2019-05-23', 10, 'blastraat', '1234AB', 'Apeldoorn', '2019-05-22')
+
+	EXEC tSQLt.AssertEqualsTable 'expected', 'TOURNAMENT';
+END;
