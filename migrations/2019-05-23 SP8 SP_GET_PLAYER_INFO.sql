abb133d8606ef19004da5efd221acd39e4bc2d2d
R0vi <ivorstaatsl0l@gmail.com>
2019-05-23 11:20:18 +0200

    made procedure and added tests

---
+++ migrations/2019-05-23 SP8 SP_GET_PLAYER_INFO.sql
@@ -0,0 +1,29 @@
+-- +migrate Up
+CREATE PROCEDURE SP_GET_PLAYER_INFO
+@playerid int
+AS
+BEGIN
+	DECLARE @orginTranCount INT
+	SET  @orginTranCount = @@TRANCOUNT
+	IF @orginTranCount > 0    
+        SAVE TRANSACTION ProcedureSave  
+    ELSE  
+        BEGIN TRANSACTION
+	BEGIN TRY		
+		SELECT * FROM PLAYER WHERE @playerid = playerid
+	END TRY
+	BEGIN CATCH
+		IF @orginTranCount = 0  
+            ROLLBACK TRANSACTION  
+        ELSE  
+			IF XACT_STATE() <> -1  
+            ROLLBACK TRANSACTION ProcedureSave
+		DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE()
+		DECLARE @ErrorSeverity INT = ERROR_SEVERITY()
+        DECLARE @ErrorState INT = ERROR_STATE()
+		RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState)
+	END CATCH
+END;
+
+-- +migrate Down
+DROP PROCEDURE SP_GET_PLAYER_INFO;
