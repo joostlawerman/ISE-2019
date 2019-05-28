5ba27851ffcde65758e706dcb2c815a2927c0ea3
Jasmijn Bartelds <jasmijn.bartelds@hotmail.nl>
2019-05-28 14:15:53 +0200

    chessclub aan tournamentname gekoppeld in IF

--- migrations/2019-05-28 SP18 SP_GET_RESULTS.sql
+++ migrations/2019-05-28 SP18 SP_GET_RESULTS.sql
@@ -17,7 +17,7 @@ BEGIN
 			BEGIN
 				RAISERROR('There is no chessclub with this name', 16, 1)
 			END
-		ELSE IF NOT EXISTS(SELECT 1 FROM TOURNAMENT WHERE tournamentname = @tournamentname)
+		ELSE IF NOT EXISTS(SELECT 1 FROM TOURNAMENT WHERE tournamentname = @tournamentname AND chessclubname = @chessclubname)
 			BEGIN
 				RAISERROR('There is no tournament with this name', 16, 1)
 			END
