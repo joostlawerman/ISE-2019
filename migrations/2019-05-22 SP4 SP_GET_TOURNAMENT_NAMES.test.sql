5c75e1d95f860c795c56c3c48630df22ad25836a
r0vi <ivorstaatsl0l@gmail.com>
2019-05-28 11:04:00 +0200

    fixed failed file save

--- migrations/2019-05-22 SP4 SP_GET_TOURNAMENT_NAMES.test.sql
+++ migrations/2019-05-22 SP4 SP_GET_TOURNAMENT_NAMES.test.sql
@@ -1,32 +1,37 @@
+-- +migrate Up
+EXEC tSQLt.NewTestClass 'SP4';
+
 -- +migrate Down
-EXEC tSQLt.DropClass 'SP14';
+EXEC tSQLt.DropClass 'SP4';
 
 -- +migrate Up
-EXEC tSQLt.NewTestClass 'SP14';
-
--- +migrate Up
-CREATE PROCEDURE [SP14].[Test get all rounds from tournament]
+CREATE PROCEDURE [SP4].[Test get all tournament names]
 AS 
 BEGIN
 	--Arrange
-	EXEC tSQLt.FakeTable 'dbo', 'TOURNAMENT_ROUND'	
-	INSERT INTO TOURNAMENT_ROUND 
-	VALUES	('cc1', 'tourny1', 1, 'roundrobin', '2019-05-28', NULL),
-			('cc1', 'tourny1', 2, 'roundrobin', '2019-05-28', NULL),  
+	EXEC tSQLt.FakeTable 'dbo', 'TOURNAMENT'	
+	INSERT INTO TOURNAMENT 
+	VALUES	(null, 'eerste tournooi', null, null, null, null, null, null, null, null, null),
+			(null, 'tweede tournooi', null, null, null, null, null, null, null, null, null)    
 
 	--Act
 	CREATE TABLE actual (
-		chessclubname  VARCHAR(100) NOT NULL,
-		tournamentname VARCHAR(100) NOT NULL,
-		roundnumber    INT          NOT NULL,
-		system         VARCHAR(25)  NOT NULL,
-		starts         DATETIME     NOT NULL,
-		ends           DATETIME     NULL
+		chessclubname        VARCHAR(100) NULL,
+		tournamentname       VARCHAR(100) NULL,
+		winner               INT          NULL,
+		contactname          VARCHAR(101) NULL,
+		starts               DATETIME     NULL,
+		ends                 DATETIME     NULL,
+		registrationfee      MONEY        NULL,
+		addressline1         VARCHAR(100) NULL,
+		postalcode           VARCHAR(6)   NULL,
+		city                 CHAR(100)    NULL,
+		registrationdeadline DATETIME     NULL,
 	)
 	
-	INSERT INTO actual EXEC SP_GET_ROUNDS
+	INSERT INTO actual (tournamentname) EXEC SP_GET_TOURNAMENT_NAMES 
  
 	--Assert
-	EXEC tSQLt.AssertEqualsTable 'TOURNAMENT_ROUND', 'actual'
+	EXEC tSQLt.AssertEqualsTable 'TOURNAMENT', 'actual'
 
 END;
