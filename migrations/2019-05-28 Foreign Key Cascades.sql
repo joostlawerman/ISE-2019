3aa46a0890c3e4b682e174d4bb5669a3252949e1
Jelmergu <github@jelmerwijnja.nl>
2019-05-28 13:50:52 +0200

    Fix migration of foreign key cascades

--- migrations/2019-05-28 Foreign Key Cascades.sql
+++ migrations/2019-05-28 Foreign Key Cascades.sql
@@ -14,16 +14,16 @@ ALTER TABLE ROUND_ROBIN_POULE DROP CONSTRAINT FK_ROUND_RO_IS_A_POUL_POULE;
 ALTER TABLE SPONSOR DROP CONSTRAINT FK_SPONSOR_SPONSOR_O_TOURNAME;
 
 -- +migrate Up
-ALTER TABLE TOURNAMENT_ROUND ADD CONSTRAINT FK_TOURNAME_TOURNAMEN_TOURNAME FOREIGN KEY (chessclubname, tournamentname) REFERENCES TOURNAMENT (chessclubname, tournamentname) ON UPDATE CASCADE
+ALTER TABLE TOURNAMENT_ROUND ADD CONSTRAINT FK_TOURNAME_TOURNAMEN_TOURNAME FOREIGN KEY (chessclubname, tournamentname) REFERENCES TOURNAMENT (chessclubname, tournamentname) ON UPDATE CASCADE;
 -- +migrate Up
-ALTER TABLE POULE ADD CONSTRAINT FK_POULE_TOURNAMEN_TOURNAME FOREIGN KEY (chessclubname, tournamentname, roundnumber) REFERENCES TOURNAMENT_ROUND (chessclubname, tournamentname, roundnumber) ON UPDATE CASCADE
+ALTER TABLE POULE ADD CONSTRAINT FK_POULE_TOURNAMEN_TOURNAME FOREIGN KEY (chessclubname, tournamentname, roundnumber) REFERENCES TOURNAMENT_ROUND (chessclubname, tournamentname, roundnumber) ON UPDATE CASCADE;
 -- +migrate Up
-ALTER TABLE TOURNAMENT_PLAYER ADD CONSTRAINT FK_TOURNAME_PLAYER_FR_TOURNAME FOREIGN KEY (chessclubname, tournamentname) REFERENCES TOURNAMENT (chessclubname, tournamentname) ON UPDATE CASCADE
+ALTER TABLE TOURNAMENT_PLAYER ADD CONSTRAINT FK_TOURNAME_PLAYER_FR_TOURNAME FOREIGN KEY (chessclubname, tournamentname) REFERENCES TOURNAMENT (chessclubname, tournamentname) ON UPDATE CASCADE;
 -- +migrate Up
-ALTER TABLE TOURNAMENT_PLAYER_OF_POULE ADD CONSTRAINT FK_TOURNAME_POULE_POULE FOREIGN KEY (chessclubname, tournamentname, roundnumber, pouleno) REFERENCES POULE (chessclubname, tournamentname, roundnumber, pouleno) ON UPDATE CASCADE
+ALTER TABLE TOURNAMENT_PLAYER_OF_POULE ADD CONSTRAINT FK_TOURNAME_POULE_POULE FOREIGN KEY (chessclubname, tournamentname, roundnumber, pouleno) REFERENCES POULE (chessclubname, tournamentname, roundnumber, pouleno) ON UPDATE CASCADE;
 -- +migrate Up
-ALTER TABLE CHESSMATCH_OF_POULE ADD CONSTRAINT FK_CHESSMAT_CHESSMATC_POULE FOREIGN KEY (chessclubname, tournamentname, roundnumber, pouleno) REFERENCES POULE (chessclubname, tournamentname, roundnumber, pouleno) ON UPDATE CASCADE
+ALTER TABLE CHESSMATCH_OF_POULE ADD CONSTRAINT FK_CHESSMAT_CHESSMATC_POULE FOREIGN KEY (chessclubname, tournamentname, roundnumber, pouleno) REFERENCES POULE (chessclubname, tournamentname, roundnumber, pouleno) ON UPDATE CASCADE;
 -- +migrate Up
-ALTER TABLE ROUND_ROBIN_POULE ADD CONSTRAINT FK_ROUND_RO_IS_A_POUL_POULE FOREIGN KEY (chessclubname, tournamentname, roundnumber, pouleno) REFERENCES POULE (chessclubname, tournamentname, roundnumber, pouleno) ON UPDATE CASCADE
+ALTER TABLE ROUND_ROBIN_POULE ADD CONSTRAINT FK_ROUND_RO_IS_A_POUL_POULE FOREIGN KEY (chessclubname, tournamentname, roundnumber, pouleno) REFERENCES POULE (chessclubname, tournamentname, roundnumber, pouleno) ON UPDATE CASCADE;
 -- +migrate Up
-ALTER TABLE SPONSOR ADD CONSTRAINT FK_SPONSOR_SPONSOR_O_TOURNAME FOREIGN KEY (chessclubname, tournamentname) REFERENCES TOURNAMENT (chessclubname, tournamentname) ON UPDATE CASCADE
+ALTER TABLE SPONSOR ADD CONSTRAINT FK_SPONSOR_SPONSOR_O_TOURNAME FOREIGN KEY (chessclubname, tournamentname) REFERENCES TOURNAMENT (chessclubname, tournamentname) ON UPDATE CASCADE;
