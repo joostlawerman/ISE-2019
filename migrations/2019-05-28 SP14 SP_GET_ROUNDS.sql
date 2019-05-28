0cc4112deaf168d9bf52fbc2286930e0d6f507eb
r0vi <ivorstaatsl0l@gmail.com>
2019-05-28 10:59:34 +0200

    SP14 made procedure and added test

---
+++ migrations/2019-05-28 SP14 SP_GET_ROUNDS.sql
@@ -0,0 +1,30 @@
+-- +migrate Down
+DROP PROCEDURE SP_GET_ROUNDS;
+
+-- +migrate Up
+CREATE PROCEDURE SP_GET_ROUNDS
+@chessclubname varchar(100),
+@tournamentname varchar(100)
+AS
+BEGIN
+	DECLARE @orginTranCount INT
+	SET  @orginTranCount = @@TRANCOUNT
+	IF @orginTranCount > 0    
+        SAVE TRANSACTION ProcedureSave  
+    ELSE  
+        BEGIN TRANSACTION
+	BEGIN TRY		
+		SELECT * FROM TOURNAMENT_ROUND WHERE @chessclubname = chessclubname AND @tournamentname = tournamentname
+	END TRY
+	BEGIN CATCH
+		IF @orginTranCount = 0  
+            ROLLBACK TRANSACTION  
+        ELSE  
+			IF XACT_STATE() <> -1  
+            ROLLBACK TRANSACTION ProcedureSave
+		DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE()
+		DECLARE @ErrorSeverity INT = ERROR_SEVERITY()
+        DECLARE @ErrorState INT = ERROR_STATE()
+		RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState)
+	END CATCH
+END;
