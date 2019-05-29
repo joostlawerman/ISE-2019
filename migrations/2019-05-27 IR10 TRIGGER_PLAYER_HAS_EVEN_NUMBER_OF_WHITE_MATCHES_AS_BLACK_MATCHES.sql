90529d2f1ca94c48af2047fee8ef0b47c0efad27
joostlawerman <joostlawerman@users.noreply.github.com>
2019-05-29 15:32:43 +0200

    IR10 implementeren TRIGGER PLAYER HAS EVEN [ch77]

--- migrations/2019-05-27 IR10 TRIGGER_PLAYER_HAS_EVEN_NUMBER_OF_WHITE_MATCHES_AS_BLACK_MATCHES.sql
+++ migrations/2019-05-27 IR10 TRIGGER_PLAYER_HAS_EVEN_NUMBER_OF_WHITE_MATCHES_AS_BLACK_MATCHES.sql
@@ -1,208 +1,119 @@
-
-begin transaction
-
-EXEC tSQLt.FakeTable 'dbo', 'CHESSMATCH'
-
-insert into CHESSMATCH
-    (chessclubname, tournamentname, roundnumber, pouleno, playeridblack, playeridwhite)
-values
-    ('Arnhemse Schaakers', 'In het Gemaal', 20, 10, 200, 100)
-;
-
-insert into CHESSMATCH
-    (chessclubname, tournamentname, roundnumber, pouleno, playeridblack, playeridwhite)
-values
-    ('Arnhemse Schaakers', 'In het Gemaal', 20, 10, 100, 200)
-;
-
-insert into CHESSMATCH
-    (chessclubname, tournamentname, roundnumber, pouleno, playeridblack, playeridwhite)
-values
-    ('Arnhemse Schaakers', 'In het Gemaal', 20, 10, 100, 400)
-;
-
-insert into CHESSMATCH
-    (chessclubname, tournamentname, roundnumber, pouleno, playeridblack, playeridwhite)
-values
-    ('Arnhemse Schaakers', 'In het Gemaal', 20, 10, 100, 300)
-;
-
-insert into CHESSMATCH
-    (chessclubname, tournamentname, roundnumber, pouleno, matchno, playeridblack, playeridwhite)
-values
-    ('Arnhemse Schaakers', 'In het Gemaal', 20, 10, 99, 100, 300)
-;
-
-insert into CHESSMATCH
-    (chessclubname, tournamentname, roundnumber, pouleno, matchno, playeridblack, playeridwhite)
-values
-    ('Arnhemse Schaakers', 'In het Gemaal', 20, 10, 900, 900, 300)
-
-;
-select 
-    *
-from CHESSMATCH cm
-;;
-
-
-select 
-    i.*
-from CHESSMATCH i
-where i.matchno = 900
-    and exists (
-        select 1
-        from CHESSMATCH cmblack
-            left join (
-                select *
-                from CHESSMATCH cm
-                where cm.chessclubname = i.chessclubname
-                    and cm.tournamentname = i.tournamentname
-                    and cm.roundnumber = i.roundnumber
-                    and cm.pouleno = i.pouleno
-                    and i.playeridblack = i.playeridblack
-            ) as cmwhite on cmwhite.chessclubname = cmblack.chessclubname
-                and cmwhite.tournamentname = cmblack.tournamentname
-                and cmwhite.roundnumber = cmblack.roundnumber
-                and cmwhite.pouleno = cmblack.pouleno
-                and cmwhite.playeridwhite = i.playeridblack
-        where cmblack.chessclubname = i.chessclubname
-            and cmblack.tournamentname = i.tournamentname
-            and cmblack.roundnumber = i.roundnumber
-            and cmblack.pouleno = i.pouleno
-            and cmblack.playeridblack = i.playeridblack
-    )
-
-
-
-;
-
--- select 1
--- from CHESSMATCH i
--- where i.matchno = 900
--- and exists (
---     select 1
---     from (
---         select 
---             cm.chessclubname,
---             cm.tournamentname,
---             cm.roundnumber,
---             cm.pouleno,
---             cm.playeridwhite as player,
---             'WHITE' as color
---         from CHESSMATCH cm
---         group by
---             cm.chessclubname, 
---             cm.tournamentname,
---             cm.roundnumber,
---             cm.pouleno,
---             cm.playeridwhite
---         union
---         select 
---             cm.chessclubname,
---             cm.tournamentname,
---             cm.roundnumber,
---             cm.pouleno,
---             cm.playeridblack as player,
---             'BLACK' as color
---         from CHESSMATCH cm
---         group by
---             cm.chessclubname, 
---             cm.tournamentname,
---             cm.roundnumber,
---             cm.pouleno,
---             cm.playeridblack
---     ) matches
---     where matches.chessclubname = i.chessclubname
---         and matches.tournamentname = i.tournamentname
---         and matches.roundnumber = i.roundnumber
---         and matches.pouleno = i.pouleno
---         and (
---             matches.player = i.playeridwhite
---         )
---     group by matches.chessclubname, 
---         matches.tournamentname,
---         matches.roundnumber,
---         matches.pouleno
---     having (SUM(IIF(matches.color = 'WHITE', 1, 0)) - SUM(IIF(matches.color = 'BLACK', 1, 0))) > 1
--- )
--- ;
-
-
-
-rollback transaction
-
-
-
-
--- -- +migrate Up
--- CREATE TRIGGER TRIGGER_PLAYER_HAS_EVEN_NUMBER_OF_WHITE_MATCHES_AS_BLACK_MATCHES
--- ON CHESSMATCH
--- AFTER INSERT, UPDATE
--- AS
---     IF @@ROWCOUNT = 0
---         RETURN
+-- +migrate Up
+CREATE TRIGGER TRIGGER_PLAYER_HAS_EVEN_NUMBER_OF_WHITE_MATCHES_AS_BLACK_MATCHES
+ON CHESSMATCH_OF_POULE
+AFTER INSERT, UPDATE
+AS
+    IF @@ROWCOUNT = 0
+        RETURN
         
---     SET NOCOUNT ON
---     BEGIN TRY
---         IF EXISTS (
---             select 1
---             from Inserted i
---             where exists (
---                 select 1
---                 from (
---                     select 
---                         cm.chessclubname,
---                         cm.tournamentname,
---                         cm.roundnumber,
---                         cm.pouleno,
---                         cm.playeridwhite as player,
---                         'WHITE' as color
---                     from CHESSMATCH cm
---                     group by
---                         cm.chessclubname, 
---                         cm.tournamentname,
---                         cm.roundnumber,
---                         cm.pouleno,
---                         cm.playeridwhite
---                     union
---                     select 
---                         cm.chessclubname,
---                         cm.tournamentname,
---                         cm.roundnumber,
---                         cm.pouleno,
---                         cm.playeridblack as player,
---                         'BLACK' as color
---                     from CHESSMATCH cm
---                     group by
---                         cm.chessclubname, 
---                         cm.tournamentname,
---                         cm.roundnumber,
---                         cm.pouleno,
---                         cm.playeridblack
---                 ) matches
---                 where matches.chessclubname = i.chessclubname
---                     and matches.tournamentname = i.tournamentname
---                     and matches.roundnumber = i.roundnumber
---                     and matches.pouleno = i.pouleno
---                     and (
---                         matches.player = i.playeridblack
---                             or matches.player = i.playeridwhite
---                     )
---                 group by matches.chessclubname, 
---                     matches.tournamentname,
---                     matches.roundnumber,
---                     matches.pouleno
---                 having (SUM(IIF(matches.color = 'WHITE', 1, 0)) - SUM(IIF(matches.color = 'BLACK', 1, 0))) > 1
---             )
---         )
---             BEGIN
---                     THROW 50000, 'A poule can not be changed when a match in that poule has started', 1
---             END
---     END TRY
---     BEGIN CATCH
---    	    THROW
---     END CATCH;
+    SET NOCOUNT ON
+    BEGIN TRY
+        IF EXISTS (
+            select 1
+            from Inserted i
+            where exists (
+                select 1
+                from (
+                    select 
+                        chessclubname,
+                        tournamentname,
+                        roundnumber,
+                        playeridwhite as player,
+                        count(*) matches,
+                        'WHITE' color
+                    from CHESSMATCH_OF_POULE
+                    group by chessclubname,
+                        tournamentname,
+                        roundnumber,
+                        playeridwhite
+                ) cmp 
+                where cmp.chessclubname = i.chessclubname
+                    and cmp.tournamentname = i.tournamentname
+                    and cmp.roundnumber = i.roundnumber
+                    and cmp.player in (i.playeridwhite, i.playeridblack)
+                    and (
+                        exists (
+                            select 1
+                            from CHESSMATCH_OF_POULE icm
+                            where icm.playeridblack = cmp.player
+                                and icm.chessclubname = cmp.chessclubname
+                                and icm.tournamentname = cmp.tournamentname
+                                and icm.roundnumber = cmp.roundnumber
+                            group by icm.chessclubname,
+                                icm.tournamentname,
+                                icm.roundnumber,
+                                icm.playeridblack
+                            having (count(*) - cmp.matches) > 1
+                        )
+                            or (
+                                not exists (
+                                    select 1
+                                    from CHESSMATCH_OF_POULE icm
+                                    where icm.playeridblack = cmp.player
+                                        and icm.chessclubname = cmp.chessclubname
+                                        and icm.tournamentname = cmp.tournamentname
+                                        and icm.roundnumber = cmp.roundnumber
+                                )
+                                and cmp.matches > 1
+                            )
+                    )
+            )
+                or exists (
+                    select 1
+                    from (
+                        select 
+                            chessclubname,
+                            tournamentname,
+                            roundnumber,
+                            playeridblack as player,
+                            count(*) matches,
+                            'BLACK' color
+                        from CHESSMATCH_OF_POULE
+                        group by chessclubname,
+                            tournamentname,
+                            roundnumber,
+                            playeridblack
+                    ) cmp 
+                    where cmp.chessclubname = i.chessclubname
+                        and cmp.tournamentname = i.tournamentname
+                        and cmp.roundnumber = i.roundnumber
+                        and cmp.player in (i.playeridwhite, i.playeridblack)
+                        and (
+                            exists (
+                                select 1
+                                from CHESSMATCH_OF_POULE icm
+                                where icm.playeridwhite = cmp.player
+                                    and icm.chessclubname = cmp.chessclubname
+                                    and icm.tournamentname = cmp.tournamentname
+                                    and icm.roundnumber = cmp.roundnumber
+                                group by icm.chessclubname,
+                                    icm.tournamentname,
+                                    icm.roundnumber,
+                                    icm.playeridwhite
+                                having (count(*) - cmp.matches) > 1
+                            )
+                                or (
+                                    not exists (
+                                        select 1
+                                        from CHESSMATCH_OF_POULE icm
+                                        where icm.playeridwhite = cmp.player
+                                            and icm.chessclubname = cmp.chessclubname
+                                            and icm.tournamentname = cmp.tournamentname
+                                            and icm.roundnumber = cmp.roundnumber
+                                    )
+                                    and cmp.matches > 1
+                                )
+                        )
+                    )
+            )
+            BEGIN
+                THROW 50000, 'One of the players has been added too many black or white matches within a poule.', 1
+            END
+    END TRY
+    BEGIN CATCH
+   	    THROW
+    END CATCH;
 
--- -- +migrate Down
--- DROP TRIGGER TRIGGER_PLAYER_HAS_EVEN_NUMBER_OF_WHITE_MATCHES_AS_BLACK_MATCHES;
+-- +migrate Down
+DROP TRIGGER TRIGGER_PLAYER_HAS_EVEN_NUMBER_OF_WHITE_MATCHES_AS_BLACK_MATCHES;
 
