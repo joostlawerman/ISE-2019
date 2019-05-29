e3f322a1e7dfc5b7655f4b6a85623cccb1a79c81
Jelmergu <github@jelmerwijnja.nl>
2019-05-29 11:05:12 +0200

    Add IR19 implementation

---
+++ migrations/Bugged/2019-05-21 IR19 TRIGGER_UNIQUE_ROUND_ROBIN_MATCH_WITHIN_POULE.test.sql
@@ -0,0 +1,59 @@
+-- +migrate Up
+EXEC tSQLt.NewTestClass 'IR19';
+
+-- +migrate Down
+EXEC tSQLt.DropClass 'IR19';
+
+-- +migrate Up
+CREATE PROCEDURE IR19.SetUp
+AS
+    BEGIN
+        EXEC tSQLt.FakeTable 'dbo.CHESSMATCH_OF_POULE'
+        EXEC tSQLt.FakeTable 'dbo.TOURNAMENT_ROUND'
+
+        EXEC [tSQLt].[ApplyTrigger] @tablename = 'dbo.CHESSMATCH_OF_POULE',
+                                    @triggername = 'TRIGGER_UNIQUE_ROUND_ROBIN_MATCH_WITHIN_POULE'
+
+        INSERT INTO TOURNAMENT_ROUND(chessclubname, tournamentname, roundnumber, system) VALUES
+            ('test', 'round', 1, 'round robin'),
+            ('test', 'brak', 1, 'bracket')
+
+        INSERT INTO CHESSMATCH_OF_POULE(chessclubname, tournamentname, roundnumber, pouleno, playeridwhite, playeridblack) VALUES
+            ('test', 'round', 1, 1, 1, 2),
+            ('test', 'round', 1, 1, 3, 4),
+            ('test', 'brak', 1, 1, 3, 4)
+    END;
+
+-- +migrate Up
+CREATE PROCEDURE sp19.test_existing_match_inverted_in_round_robin_poule AS
+BEGIN
+
+    EXEC tSQLt.ExpectException 'There is a match that is duplicate in a round robin poule'
+    -- Execute
+
+    INSERT INTO CHESSMATCH_OF_POULE (chessclubname, tournamentname, roundnumber, pouleno, playeridwhite, playeridblack) VALUES
+            ('test', 'round', 1, 1, 2, 1)
+END;
+
+-- +migrate Up
+CREATE PROCEDURE sp19.test_duplicate_match_in_round_robin_poule AS
+BEGIN
+
+    EXEC tSQLt.ExpectException 'There is a match that is duplicate in a round robin poule'
+    -- Execute
+
+    INSERT INTO CHESSMATCH_OF_POULE (chessclubname, tournamentname, roundnumber, pouleno, playeridwhite, playeridblack) VALUES
+            ('test', 'round', 1, 1, 1, 2)
+END;
+
+-- +migrate Up
+CREATE PROCEDURE sp19.test_bracket_doesnt_trigger AS
+BEGIN
+
+    EXEC tSQLt.ExpectException 'There is a match that is duplicate in a round robin poule'
+    -- Execute
+
+    INSERT INTO CHESSMATCH_OF_POULE (chessclubname, tournamentname, roundnumber, pouleno, playeridwhite, playeridblack) VALUES
+            ('test', 'round', 1, 1, 1, 2)
+END;
+
