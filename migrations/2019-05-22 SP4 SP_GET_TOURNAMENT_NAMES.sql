6f7d44ce3705b0265f866f4d0c6511e10f784cce
Jelmergu <github@jelmerwijnja.nl>
2019-06-03 10:48:58 +0200

    Add chessclubname to SP4

--- migrations/2019-05-22 SP4 SP_GET_TOURNAMENT_NAMES.sql
+++ migrations/2019-05-22 SP4 SP_GET_TOURNAMENT_NAMES.sql
@@ -3,26 +3,33 @@ DROP PROCEDURE SP_GET_TOURNAMENT_NAMES;
 
 -- +migrate Up
 CREATE PROCEDURE SP_GET_TOURNAMENT_NAMES
+        @chessclubname VARCHAR(100)
 AS
-BEGIN
-	DECLARE @orginTranCount INT
-	SET  @orginTranCount = @@TRANCOUNT
-	IF @orginTranCount > 0    
-        SAVE TRANSACTION ProcedureSave  
-    ELSE  
-        BEGIN TRANSACTION
-	BEGIN TRY		
-		SELECT tournamentname FROM TOURNAMENT 
-	END TRY
-	BEGIN CATCH
-		IF @orginTranCount = 0  
-            ROLLBACK TRANSACTION  
-        ELSE  
-			IF XACT_STATE() <> -1  
-            ROLLBACK TRANSACTION ProcedureSave
-		DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE()
-		DECLARE @ErrorSeverity INT = ERROR_SEVERITY()
+    BEGIN
+        DECLARE @orginTranCount INT
+        SET @orginTranCount = @@TRANCOUNT
+        IF @orginTranCount > 0
+            SAVE TRANSACTION ProcedureSave
+        ELSE
+            BEGIN TRANSACTION
+        BEGIN TRY
+        IF @chessclubname IS NULL OR NOT EXISTS(SELECT 1 FROM chessclub WHERE chessclubname = @chessclubname)
+            SELECT chessclubname, tournamentname
+            FROM TOURNAMENT
+        ELSE
+            SELECT chessclubname, tournamentname
+            FROM TOURNAMENT
+            WHERE chessclubname = @chessclubname
+        END TRY
+        BEGIN CATCH
+        IF @orginTranCount = 0
+            ROLLBACK TRANSACTION
+        ELSE
+            IF XACT_STATE() <> -1
+                ROLLBACK TRANSACTION ProcedureSave
+        DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE()
+        DECLARE @ErrorSeverity INT = ERROR_SEVERITY()
         DECLARE @ErrorState INT = ERROR_STATE()
-		RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState)
-	END CATCH
-END;
+        RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState)
+        END CATCH
+    END;
