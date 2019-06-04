a1fb6abe7f016e65f2727699496d8f59ad6cf07f
LucaHogeweide <lucahogeweide@gmail.com>
2019-06-04 11:50:21 +0200

    Added SP11 with tests.

---
+++ migrations/2019-06-04 SP11 SP_GET_SYSTEMS.sql
@@ -0,0 +1,33 @@
+-- +migrate Up
+CREATE PROC SP_GET_SYSTEMS
+AS
+BEGIN
+	DECLARE @orginTranCount INT
+	SET  @orginTranCount = @@TRANCOUNT
+	IF @orginTranCount > 0    
+        SAVE TRANSACTION ProcedureSave  
+    ELSE  
+        BEGIN TRANSACTION  
+	BEGIN TRY		
+		
+		SELECT system
+		FROM SYSTEMS
+				
+        IF @orginTranCount = 0  
+            COMMIT TRANSACTION 
+	END TRY
+	BEGIN CATCH
+		IF @orginTranCount = 0  
+            ROLLBACK TRANSACTION  
+        ELSE  
+			IF XACT_STATE() <> -1  
+            ROLLBACK TRANSACTION ProcedureSave  
+		DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE()
+		DECLARE @ErrorSeverity INT = ERROR_SEVERITY()
+        DECLARE @ErrorState INT = ERROR_STATE()
+		RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState)
+	END CATCH
+END;
+
+-- +migrate Down
+Drop proc SP_GET_SYSTEMS;
