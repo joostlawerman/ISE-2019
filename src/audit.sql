1d36845d3f42339c75f88a7308bcf5307a1a37b8
joostlawerman <joostlawerman@users.noreply.github.com>
2019-06-05 15:03:02 +0200

    Code gen for trigger

--- src/audit.sql
+++ src/audit.sql
@@ -1,43 +1,39 @@
-
-
-create trigger change_in___table_name__
-on __table_name__
-    AFTER INSERT, UPDATE, DELETE
+select '
+create TRIGGER TRIGGER_AUDIT_CHANGE_IN_' + TABLE_NAME  + ' 
+on ' + TABLE_NAME  + '
+    AFTER INSERT, UPDATE, DELETE 
 AS
     BEGIN
         IF @@ROWCOUNT = 0
             RETURN
         BEGIN TRY
-            DECLARE @action VARCHAR(255);
+            DECLARE @action VARCHAR(255)
 
             IF EXISTS (SELECT * FROM DELETED) AND EXISTS  (SELECT * FROM INSERTED)
                 BEGIN
-                    -- Update
-                    SET @action = 'UPDATE'
+                    SET @action = ''UPDATE''
                 END
             ELSE IF EXISTS (SELECT * FROM INSERTED)
                 BEGIN
-                    -- Create 
-                    SET @action = 'INSERT'
+                    SET @action = ''INSERT''
                 END
             ELSE IF EXISTS (SELECT * FROM DELETED)
                 BEGIN
-                    -- Delete
-                    SET @action = 'DELETE'
+                    SET @action = ''DELETE''
                 END
             ELSE 
                 BEGIN
-                    THROW 50000, 'Illigal action', 1
+                    THROW 50000, ''Illigal action'', 1
                 END
 
-            INSERT INTO (
+            INSERT INTO AUDITLOG (
                 tablename,
 	            [user],
 	            [action],
             	[timestamp]
             ) VALUES (
-                '__table_name__'
-                select CURRENT_USER,
+                ''' + TABLE_NAME  + ''',
+                CURRENT_USER,
                 @action,
                 CURRENT_TIMESTAMP
             )
@@ -45,11 +41,8 @@ AS
         BEGIN CATCH
             THROW
         END CATCH
-    END;
-
-
-
-select *
+    END
+'
 from INFORMATION_SCHEMA.TABLES
 where TABLE_CATALOG = 'ISE_2019'
     and TABLE_SCHEMA = 'dbo'
