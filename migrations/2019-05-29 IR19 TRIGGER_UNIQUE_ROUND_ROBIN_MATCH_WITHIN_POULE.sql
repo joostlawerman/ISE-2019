5509dbbf4e01e7d958df83f2ab289bee498872df
Jelmergu <github@jelmerwijnja.nl>
2019-06-06 14:28:43 +0200

    Fix IR19
    Signed-off-by: Jelmergu <github@jelmerwijnja.nl>

--- migrations/2019-05-29 IR19 TRIGGER_UNIQUE_ROUND_ROBIN_MATCH_WITHIN_POULE.sql
+++ migrations/2019-05-29 IR19 TRIGGER_UNIQUE_ROUND_ROBIN_MATCH_WITHIN_POULE.sql
@@ -9,82 +9,43 @@ AS
         BEGIN TRY
 
         IF EXISTS(SELECT 1
-                  FROM
-                      (
-                           /*(
-                             (
-                                  SELECT
-                                      i.chessclubname,
-                                      i.tournamentname,
-                                      i.roundnumber,
-                                      i.pouleno,
-                                      concat(playeridblack, '-', playeridwhite) AS match
-                                  FROM inserted i
-                                      INNER JOIN TOURNAMENT_ROUND tr ON tr.tournamentname = i.tournamentname AND
-                                                                        tr.chessclubname = i.chessclubname AND
-                                                                        tr.roundnumber = i.roundnumber
-                                  WHERE playeridwhite > playeridblack AND tr.system = 'round robin'
-                              )
-                              UNION ALL
-                              (
-                                  SELECT
-                                      i.chessclubname,
-                                      i.tournamentname,
-                                      i.roundnumber,
-                                      i.pouleno,
-                                      concat(i.playeridwhite, '-', playeridblack) AS match
-                                  FROM inserted i
-                                      INNER JOIN TOURNAMENT_ROUND tr ON tr.tournamentname = i.tournamentname AND
-                                                                        tr.chessclubname = i.chessclubname AND
-                                                                        tr.roundnumber = i.roundnumber
-                                  WHERE playeridwhite < playeridblack AND tr.system = 'round robin'
-                              )
-                          )
-                          UNION ALL */
-                          (
-                              (
-                                  SELECT
-                                      cp.chessclubname,
-                                      cp.tournamentname,
-                                      cp.roundnumber,
-                                      cp.pouleno,
-                                      concat(cp.playeridblack, '-', cp.playeridwhite) AS match
-                                  FROM CHESSMATCH_OF_POULE cp
-                                      INNER JOIN inserted i ON cp.tournamentname = i.tournamentname AND
-                                                               cp.chessclubname = i.chessclubname AND
-                                                               cp.roundnumber = i.roundnumber AND
-                                                               cp.pouleno = i.pouleno
-                                      INNER JOIN TOURNAMENT_ROUND tr ON tr.tournamentname = i.tournamentname AND
-                                                                        tr.chessclubname = i.chessclubname AND
-                                                                        tr.roundnumber = i.roundnumber
-                                  WHERE cp.playeridwhite > cp.playeridblack AND tr.system = 'round robin'
-                              )
-                              UNION ALL
-                              (
-                                  SELECT
-                                      cp.chessclubname,
-                                      cp.tournamentname,
-                                      cp.roundnumber,
-                                      cp.pouleno,
-                                      concat(cp.playeridwhite, '-', cp.playeridblack) AS match
-                                  FROM CHESSMATCH_OF_POULE cp
-                                      INNER JOIN inserted i ON cp.tournamentname = i.tournamentname AND
-                                                               cp.chessclubname = i.chessclubname AND
-                                                               cp.roundnumber = i.roundnumber AND
-                                                               cp.pouleno = i.pouleno
-
-                                      INNER JOIN TOURNAMENT_ROUND tr ON tr.tournamentname = i.tournamentname AND
-                                                                        tr.chessclubname = i.chessclubname AND
-                                                                        tr.roundnumber = i.roundnumber
-                                  WHERE cp.playeridwhite < cp.playeridblack AND tr.system = 'round robin'
-                              )
-                          )
-                      ) uni
-                  GROUP BY chessclubname, tournamentname, roundnumber, pouleno, match
-                  HAVING COUNT(MATCH) > 1)
+                  FROM inserted cp
+                      INNER JOIN inserted i ON cp.tournamentname = i.tournamentname AND
+                                               cp.chessclubname = i.chessclubname AND
+                                               cp.roundnumber = i.roundnumber AND
+                                               cp.pouleno = i.pouleno AND
+                                               i.playeridblack IN (cp.playeridblack, cp.playeridwhite) AND
+                                               i.playeridwhite IN (cp.playeridblack, cp.playeridwhite)
+                      INNER JOIN TOURNAMENT_ROUND tr ON tr.tournamentname = i.tournamentname AND
+                                                        tr.chessclubname = i.chessclubname AND
+                                                        tr.roundnumber = i.roundnumber
+                  WHERE (cp.playeridwhite < cp.playeridblack OR cp.playeridwhite > cp.playeridblack) AND
+                        tr.system = 'round robin'
+                  HAVING count(*) > 1)
             BEGIN
-                THROW 50001, 'There is a match that is duplicate in a round robin poule', 1
+                THROW 50001, 'The inserted values have a duplicate match in this round robin poule', 1
             END
+
+        /**
+            Check of een nieuwe rij geen duplicate heeft met al bestaande rijen
+         */
+        IF EXISTS(SELECT 1
+                  FROM CHESSMATCH_OF_POULE cp
+                      INNER JOIN inserted i ON cp.tournamentname = i.tournamentname AND
+                                               cp.chessclubname = i.chessclubname AND
+                                               cp.roundnumber = i.roundnumber AND
+                                               cp.pouleno = i.pouleno AND
+                                               i.playeridblack IN (cp.playeridblack, cp.playeridwhite) AND
+                                               i.playeridwhite IN (cp.playeridblack, cp.playeridwhite)
+                      INNER JOIN TOURNAMENT_ROUND tr ON tr.tournamentname = i.tournamentname AND
+                                                        tr.chessclubname = i.chessclubname AND
+                                                        tr.roundnumber = i.roundnumber
+                  WHERE (cp.playeridwhite < cp.playeridblack OR cp.playeridwhite > cp.playeridblack) AND
+                        tr.system = 'round robin')
+            BEGIN
+                THROW 50001, 'One of the matches that are inserted already exists in this round robin poule', 1
+            END
+
         END TRY
         BEGIN CATCH
         THROW
