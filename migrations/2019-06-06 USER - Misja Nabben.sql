9cf017fa4ad4758addd952d9959185402ceb7ac6
joostlawerman <joostlawerman@users.noreply.github.com>
2019-06-06 13:14:52 +0200

    Added commands and users

---
+++ migrations/2019-06-06 USER - Misja Nabben.sql
@@ -0,0 +1,15 @@
+-- +migrate Up
+CREATE LOGIN MisjaNabben   
+    WITH PASSWORD = 'i2fi8Y8rkG4T2y7zT69oVqE97qY';  
+
+-- +migrate Up  
+CREATE USER MisjaNabben FOR LOGIN MisjaNabben;  
+
+-- +migrate Up
+GRANT EXECUTE TO MisjaNabben;
+
+-- +migrate Down
+DROP USER MisjaNabben;
+
+-- +migrate Down
+DROP LOGIN MisjaNabben;
