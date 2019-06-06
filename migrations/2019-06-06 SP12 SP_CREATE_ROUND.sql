6c9bd27f279570c28d604eb43c4e28380ec94190
Jasmijn Bartelds <jasmijn.bartelds@hotmail.nl>
2019-06-06 15:46:46 +0200

    ch78 create round

--- migrations/2019-06-06 SP12 SP_CREATE_ROUND.sql
+++ migrations/2019-06-06 SP12 SP_CREATE_ROUND.sql
@@ -5,7 +5,10 @@ DROP PROC SP_CREATE_ROUND;
 CREATE PROC SP_CREATE_ROUND (
 	@chessclubname varchar(100),
 	@tournamentname varchar(100),
-	@roundnumber int)
+	@system varchar(25),
+	@starts datetime,
+	@ends datetime = NULL,
+	@isFinal bit)
 AS
 BEGIN
 	DECLARE @orginTranCount INT
@@ -16,7 +19,25 @@ BEGIN
         BEGIN TRANSACTION  
 	BEGIN TRY
 		
-		
+		IF NOT EXISTS(SELECT 1 FROM CHESSCLUB WHERE chessclubname = @chessclubname)
+			BEGIN
+				RAISERROR('There is no chessclub with this name', 16, 1)
+			END
+		IF NOT EXISTS(SELECT 1 FROM TOURNAMENT WHERE tournamentname = @tournamentname)
+			BEGIN
+				RAISERROR('There is no tournament with this name', 16, 1)
+			END
+		IF (@system != 'round robin')
+			BEGIN
+				RAISERROR('The system currently only supports round robin', 16, 1)
+			END	
+			
+		DECLARE @roundnumber int = (ISNULL((SELECT TOP 1 roundnumber FROM TOURNAMENT_ROUND WHERE chessclubname = @chessclubname AND tournamentname = @tournamentname ORDER BY roundnumber DESC),  0)) + 1
+
+		INSERT INTO TOURNAMENT_ROUND
+		VALUES (@chessclubname, @tournamentname, @roundnumber, @system, @starts, @ends)
+
+		EXEC SP_CREATE_POULES @chessclubname, @tournamentname, @roundnumber, @isFinal
 				
         IF @orginTranCount = 0  
             COMMIT TRANSACTION 
--- migrations/2019-06-06 SP12 SP_CREATE_ROUND.test.sql
+++ migrations/2019-06-06 SP12 SP_CREATE_ROUND.test.sql
@@ -3,3 +3,146 @@ EXEC tSQLt.NewTestClass 'SP12';
 
 -- +migrate Down
 EXEC tSQLt.DropClass 'SP12';
+
+-- +migrate Up
+CREATE PROCEDURE SP12.SetUp
+AS
+BEGIN
+   	--Arrange
+		EXEC tSQLt.FakeTable 'dbo', 'CHESSCLUB'
+	INSERT INTO CHESSCLUB 
+	VALUES	('Schaakvereniging Horst', null, null, null, null),	
+			('Schaakvereniging Deventer', null, null, null, null),
+			('Schaakvereniging Apeldoorn', null, null, null, null),
+			('Schaakvereniging Arnhem', null, null, null, null),
+			('Schaakvereniging Groningen', null, null, null, null),
+			('Schaakvereniging Almere', null, null, null, null)
+
+	EXEC tSQLt.FakeTable 'dbo', 'PLAYER'
+	INSERT INTO PLAYER 
+	VALUES	(1, 'Schaakvereniging Horst', null, null, null, null, null, null, null, null),
+			(2, 'Schaakvereniging Horst', null, null, null, null, null, null, null, null),
+			(3, 'Schaakvereniging Horst', null, null, null, null, null, null, null, null),
+			(4, 'Schaakvereniging Deventer', null, null, null, null, null, null, null, null),
+			(5, 'Schaakvereniging Deventer', null, null, null, null, null, null, null, null),
+			(6, 'Schaakvereniging Apeldoorn', null, null, null, null, null, null, null, null),
+			(7, 'Schaakvereniging Apeldoorn', null, null, null, null, null, null, null, null),
+			(8, 'Schaakvereniging Arnhem', null, null, null, null, null, null, null, null),
+			(9, 'Schaakvereniging Arnhem', null, null, null, null, null, null, null, null),
+			(10, 'Schaakvereniging Arnhem', null, null, null, null, null, null, null, null),
+			(11, 'Schaakvereniging Groningen', null, null, null, null, null, null, null, null),
+			(12, 'Schaakvereniging Groningen', null, null, null, null, null, null, null, null),
+			(13, 'Schaakvereniging Almere', null, null, null, null, null, null, null, null),
+			(14, 'Schaakvereniging Almere', null, null, null, null, null, null, null, null),
+			(15, 'Schaakvereniging Almere', null, null, null, null, null, null, null, null)
+
+	EXEC tSQLt.FakeTable 'dbo', 'TOURNAMENT'
+	INSERT INTO TOURNAMENT VALUES ('Schaakvereniging Horst', 'Eerste schaaktoernooi', null, null, null, null, null, null, null, null, null)	
+	
+	EXEC tSQLt.FakeTable 'dbo', 'TOURNAMENT_PLAYER'
+	INSERT INTO TOURNAMENT_PLAYER
+	VALUES	('Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, null),
+			('Schaakvereniging Horst', 'Eerste schaaktoernooi', 2, null),
+			('Schaakvereniging Horst', 'Eerste schaaktoernooi', 3, null),
+			('Schaakvereniging Horst', 'Eerste schaaktoernooi', 4, null),
+			('Schaakvereniging Horst', 'Eerste schaaktoernooi', 5, null),
+			('Schaakvereniging Horst', 'Eerste schaaktoernooi', 6, null),
+			('Schaakvereniging Horst', 'Eerste schaaktoernooi', 7, null),
+			('Schaakvereniging Horst', 'Eerste schaaktoernooi', 8, null),
+			('Schaakvereniging Horst', 'Eerste schaaktoernooi', 9, null),
+			('Schaakvereniging Horst', 'Eerste schaaktoernooi', 10, null),
+			('Schaakvereniging Horst', 'Eerste schaaktoernooi', 11, null),
+			('Schaakvereniging Horst', 'Eerste schaaktoernooi', 12, null),
+			('Schaakvereniging Horst', 'Eerste schaaktoernooi', 13, null),
+			('Schaakvereniging Horst', 'Eerste schaaktoernooi', 14, null),
+			('Schaakvereniging Horst', 'Eerste schaaktoernooi', 15, null)
+
+	EXEC tSQLt.FakeTable 'dbo', 'TOURNAMENT_PLAYER_OF_POULE'
+
+	EXEC tSQLt.FakeTable 'dbo', 'POULE'
+
+	EXEC tSQLt.FakeTable 'dbo', 'TOURNAMENT_ROUND' 
+
+	EXEC tSQLt.FakeTable 'dbo', 'CHESSMATCH_OF_POULE' 
+	INSERT INTO CHESSMATCH_OF_POULE
+	VALUES	(1, 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, 1, 1, 4, 'black'),
+			(2, 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, 1, 2, 3, 'white'),
+			(3, 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, 1, 3, 1, 'remise'),
+			(4, 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, 1, 4, 2, 'black'),
+			(5, 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, 1, 1, 2, 'remise'),
+			(6, 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, 1, 3, 4, 'black'),
+			(7, 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, 2, 5, 8, 'black'),
+			(8, 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, 2, 6, 7, 'white'),
+			(9, 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, 2, 7, 5, 'remise'),
+			(10, 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, 2, 8, 6, 'black'),
+			(11, 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, 2, 5, 6, 'black'),
+			(12, 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, 2, 7, 8, 'white'),
+			(13, 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, 3, 9, 12, 'remise'),
+			(14, 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, 3, 10, 11, 'black'),
+			(15, 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, 3, 11, 9, 'black'),
+			(15, 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, 3, 12, 10, 'black'),
+			(15, 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, 3, 9, 10, 'black'),
+			(15, 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, 3, 11, 12, 'black'),
+			(16, 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, 4, 13, 15, 'white'),
+			(17, 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, 4, 15, 14, 'black'),
+			(18, 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, 4, 14, 13, 'black')
+END;
+
+-- +migrate Up
+CREATE PROCEDURE [SP12].[Test a round is created]
+AS 
+BEGIN
+	--Act
+	EXEC SP_CREATE_ROUND 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 'round robin', '2018-06-16 10:53:07.380', '2018-06-20 10:53:07.380', 0
+
+	CREATE TABLE expected (
+		chessclubname  VARCHAR(100) NOT NULL,
+		tournamentname VARCHAR(100) NOT NULL,
+		roundnumber    INT          NOT NULL,
+		system         VARCHAR(25)  NOT NULL,
+		starts         DATETIME     NOT NULL,
+		ends           DATETIME     NULL
+	)
+
+	INSERT INTO expected 
+	VALUES ('Schaakvereniging Horst', 'Eerste schaaktoernooi', 1, 'round robin', '2018-06-16 10:53:07.380', '2018-06-20 10:53:07.380')
+
+	--Assert
+	EXEC tSQLt.AssertEqualsTable expected, TOURNAMENT_ROUND
+END;
+
+-- +migrate Up
+CREATE PROCEDURE [SP12].[Test if a chessclub does not exist]
+AS 
+BEGIN
+	--Act
+	EXEC tSQLt.ExpectException @ExpectedMessage = 'There is no chessclub with this name'
+
+	--Assert
+	EXEC SP_CREATE_ROUND 'Schaakvereniging bestaatniet', 'Eerste schaaktoernooi', 'round robin', '2018-06-16 10:53:07.380', '2018-06-20 10:53:07.380', 0
+
+END;
+
+-- +migrate Up
+CREATE PROCEDURE [SP12].[Test if a tournament name does not exist]
+AS 
+BEGIN
+	--Act
+	EXEC tSQLt.ExpectException @ExpectedMessage = 'There is no tournament with this name'
+
+	--Assert
+		EXEC SP_CREATE_ROUND 'Schaakvereniging Horst', 'Tweede schaaktoernooi', 'round robin', '2018-06-16 10:53:07.380', '2018-06-20 10:53:07.380', 0
+
+END;
+
+-- +migrate Up
+CREATE PROCEDURE [SP12].[Test if a system does not exist]
+AS 
+BEGIN
+	--Act
+	EXEC tSQLt.ExpectException @ExpectedMessage = 'The system currently only supports round robin'
+
+	--Assert
+		EXEC SP_CREATE_ROUND 'Schaakvereniging Horst', 'Eerste schaaktoernooi', 'brack', '2018-06-16 10:53:07.380', '2018-06-20 10:53:07.380', 0
+
+END;
